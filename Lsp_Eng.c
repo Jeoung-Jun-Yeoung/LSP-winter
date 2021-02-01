2ed123d6254ce561b623e17ff46c927f37a4d00d
jun-yeoung <dbahsmssk@naver.com>
2021-02-01 19:43:38 +0900

    20200201

--- Lsp_Eng.c
+++ Lsp_Eng.c
@@ -5,88 +5,13 @@
 
 typedef struct Node{
 
-	char eng[20];
-	char kor[2][20];
+	char* eng;
+	char* kor[3];
 	struct Node* next;
 
 } Node;
 
-
 int file_num = 0;
-
-
-   int main(void){
-   
-  int print_num = 0;
-  int play_mode = 0;
-
-	   
-	   while(1){
-		
-   
-      printf(" >>영어 단어 암기 프로그램<< \n");
-	  blank();
-	  
-	  printf("1. 영어 단어 맞추기	2. 플래쉬 카드\n");
-	  blank();
-	 
-	  printf("3. 행맨(hangman)	4. 단어장 관리\n");
-	  blank();
-     
-	  printf("5. 프로그램 종료\n");
-	  printf("\n");
-
-
-      printf("번호를 선택하세요 : \n");
-	  scanf ("%d", &play_mode);
-
-	  printf("파일 번호를 선택하세요 : \n");
-	  scanf("%d", &file_num);
-	  
-	  printf("출력방식(알파벳 순서대로 :1, 무작위 :2)\n");
-	  scanf("%d", &print_num);
-
-
-
-	switch(play_mode){
-		
-		case 1 : 
-			
-		First_Function();
-		
-		break;
-
-      	case 2 :
-
-		Second_Function();
-	   
-		break;
-
-		case 3 :
-			
-		Third_Function();
-		
-		break;
-
-		case 4 :
-
-		Fourth_Function();
-		
-		break;
-
-		case 5 :
-
-		return 0 ;
-			
-		default : 
-			printf("유효한 숫자가 아닙니다. 1 ~ 5만 입력해 주세요");
-			sleep(5);
-		
-		}
-	}
-  
-  }
-
 
 
 void blank(){
@@ -97,166 +22,231 @@ void blank(){
 
 
 
-void First_Function(){
+Node* make_node(int file_num){
 
-int count = 0;
+	FILE *fp;
+
+	int count = 0;
+
+	char buffer[80];
+	char ch;
+
+	Node* head = (Node*)malloc(sizeof(Node));
+	
+	if (file_num == 1){
+
+	fp = fopen("1.dic","rt");
+
+	} else if (file_num == 2){
+
+		fp = fopen("2.dic","rt");
+
+	} else if(file_num == 3){
+
+		fp = fopen("3.dic","rt");
+	}
 
 
-blank();
-blank();
-
-printf(">> 영어단어 암기 프로그램 : 영어단어 맞추기 <<");
-
-
-blank();
-blank();
-
-// open file num -> file_num,  global value
-
-void word_count(){
-  
-      int count = 0;
-      FILE *fp;
-	  char cha;
-  
-
-	 printf("파일번호확인 %d \n", file_num);
-     
-  if (file_num == 1){
-
-	Node* word = (Node*)malloc(sizeof(Node)); 
-
-    fp = fopen("1.dic","rt");
-  
 	if(fp == NULL){
 
+		printf("파일을 읽지 못했습니다.");
 
-	printf("파일읽기에 실패했습니다.");
-	
-	return 0;
-	
 	}
 
 
-    while(feof(fp) == 0) {
-
-	fscanf(fp,"%c",word->eng);
-
-	
-	if (cha == ' '){
-	
-	
-	int kor_count = 0;
-
-	fscanf(fp,"%c",word->kor[kor_count]);
-	
-	kor_count++;
-	
-	}
+	while(feof(fp) == 0){
 
 
-	printf("%s\n",word->eng);
-	
-	printf("%c",cha);
+		int index = 0;
 
-	
+		Node* node = (Node*)malloc(sizeof(Node));
 
-	if (cha == '\n'){ //다음 단어가 있으면 노드를 만들어서 연결.
-	
-		count++;
+
+		fgets(buffer,sizeof(buffer),fp);
+
+		buffer[sizeof(buffer) -1] = '\0';
+
+		char *token = strtok(buffer," ");
+
+		while(token != NULL){
+			
+
+			if(index == 0){
+
+				node->eng = token;
+
+			}
+
+			else {
+
+				node->kor[index - 1] = token;
+
+			}
+
 		
-		Node* next_node = (Node*)malloc(sizeof(Node));
+			token = strtok(NULL," ");
+			index++;
 
-		next_node->next = NULL;
+		}
 
-		word->next = next_node;
+		node->next = head->next;
+
+		printf("%s\n",node->eng);
+		printf("%s\n",node->kor[0]);
+
+		head->next = node;
 
 	}
 
-	}
-
-	printf("단어갯수 %d \n", count);
 	fclose(fp);
-  }
+/*
+	int c = 30;
+	Node *curr = head;
 
+	while(c != 0){
 
-  else if(file_num == 2){
+	printf("%s ",curr->eng);
 
-	fp = fopen("2.dic","rt");
- 
-     if(fp == NULL){
- 
-     
-     printf("파일읽기에 실패했습니다.");
-     
-     return 0;
-     
-     }
-     
-     while(feof(fp) == 0) {
- 
-     fscanf(fp,"%c",&cha);
-     printf("%c",cha);
- 
-     if(cha == '\n'){
-     
-         count++;
-     
-     }
-     
-     }
- 
-     printf("단어갯수 %d \n", count);
- 
-	 fclose(fp);
-   }
+	printf("%s ",curr->kor[0]);
+	c--;
+	curr = curr->next;
+	}*/
+	return head;
 
-	else if(file_num == 3){
-
-	fp = fopen("3.dic","rt");
- 
-     if(fp == NULL){
-      
-     printf("파일읽기에 실패했습니다.");
-     
-     return 0;
-     
-     }
-     
-     while(feof(fp) == 0) {
- 
-     fscanf(fp,"%c",&cha);
-   
-	 printf("%c",cha);
- 
-     if(cha == '\n'){
-     
-         count++;
-     
-     }
-     
-     }
- 
-     printf("단어갯수 %d \n", count);
- 	 
-	 fclose(fp);
- 
-	}
+//	return count;
 
 }
 
-printf("word count 함수 실행전 \n");
+/*
 
-word_count();
+   void Make_linkedlist(){
 
-printf("word count 실행 됨 \n");
+
+   int count = 0;
+
+//	char buffer[80];
+
+FILE *fp;
+
+count = Count_word();
+
+
+Node* head_node = (Node*)malloc(sizeof(Node) * count);
+
+// fgets 로 한줄씩 받고, strok거르자.
+
+
+fopen("1.dic","rt");
+
+
+while(count != 0){
+
+count--;
+
+char buffer[80];
+fgets(buffer,sizeof(buffer),fp);
+
+//	buffer[strlen(buffer) - 1] = '\0';
+
+
+printf("%s",buffer);
+
+
+}
+
+printf("끝");
+
+fclose(fp);
+
+
+
+}
+ */
+
+/*
+   void Getc(){
+
+
+
+   FILE *fp = fopen("1.dic","rt");
+
+   int c = 0;
+
+   if (fp == NULL){
+
+   printf("파일을 읽을 수 없습니다.");
+   }
+
+   while((c = fgetc(fp)) != EOF){
+
+   Node* node = (Node*)malloc(sizeof(Node));	
+
+   if((c = fgetc(fp)) == ' '){
+
+   while ((c = fgetc(fp)) != ' ' ){
+
+
+   node->kor = c;
+
+   }
+   }
+
+   else {
+
+   node->eng = c;
+
+   }
+
+   }
+
+
+
+
+   }
+ */
+
+
+void Sorting(Node* node){
+
+
+
+
+
+
+
+}
+
+void First_Function(int file_num){
+
+	blank();
+	blank();
+	blank();
+
+	printf(">> 영어단어 암기 프로그램 : 영어단어 맞추기 <<");
+
+
+	blank();
+	blank();
+
+	Node* node = (Node*)malloc(sizeof(Node));
+
+	node = make_node(file_num);
+
+
+//	printf("%s", node->next->next->kor[0]);
+
 
 
 }
 
 void Second_Function(){
 
-printf("2번프로그램입니다.");
+
+	//	make_node();
+
+	char g = 'A';
+
+	printf("%40c",g);
 
 }
 
@@ -264,7 +254,7 @@ printf("2번프로그램입니다.");
 void Third_Function(){
 
 
-printf("3번 프로그램 입니다.");
+	printf("3번 프로그램 입니다.");
 
 }
 
@@ -273,8 +263,94 @@ printf("3번 프로그램 입니다.");
 void Fourth_Function(){
 
 
-printf("4번 프로그램 입니다.");
+	printf("4번 프로그램 입니다.");
 
 }
 
+int main(void){
+
+	int print_num = 0;
+	int play_mode = 0;
+	int file_num = 0;
+	int sec = 7;
+
+
+	while(1){
+
+
+		printf(" >>영어 단어 암기 프로그램<< \n");
+		blank();
+
+		printf("1. 영어 단어 맞추기 2. 플래쉬 카드\n");
+		blank();
+
+		printf("3. 행맨(hangman)    4. 단어장 관리\n");
+		blank();
+
+		printf("5. 프로그램 종료\n");
+		blank();
+
+
+		printf("번호를 선택하세요 : ");
+		scanf ("%d", &play_mode);
+
+		blank();
+
+		printf("파일명(일차) : ");
+		scanf("%d", &file_num);
+
+		blank();
+
+		printf("출력방식(알파벳 순서대로 :1 , 무작위 : 2) : ");
+		scanf("%d", &print_num);
+
+		if(play_mode == 2){
+
+			blank();
+
+			printf("속도(초) : ");
+			scanf("%d", &sec);
+
+		}
+
+		blank();
+
+
+		switch(play_mode){
+
+			case 1 :
+
+				First_Function(file_num);
+
+				system("clear");
+
+				break;
+
+			case 2 :
+
+				Second_Function();
+
+				break;
+
+			case 3 :
+
+				Third_Function();
+
+				break;
+
+			case 4 :
+
+				Fourth_Function();
+
+				break;
+			case 5 :
+				return 0 ;
+
+			default :
+				printf("유효한 숫자가 아닙니다. 1 ~ 5만 입력해 주세요");
+
+		}
+	}
+
+}   
 
